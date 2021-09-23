b6861a7b3a1f5efbdebebdb92d72a66801659d92
Bernardo Salgueiro <bernardo.roll@gmail.com>
2021-09-23 17:25:47 -0300

    Arrow Functions

---
+++ arrow-function.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+  const button = document.getElementById('button');
+
+  button.addEventListener('click', () => {
+    alert('PEGADINHA DO MALANDRO! IÉ IÉ!!! 🤪');
+  });
+};
+
+const funcao = (num1, num2) => num1 + num2;
+
+console.log(funcao(2, 8));
