93b0b8af792959ae30416c463dcfe11481d42a8b
Bernardo Salgueiro <bernardo.roll@gmail.com>
2021-09-27 17:29:31 -0300

    Object assign

--- assign.js
+++ assign.js
@@ -3,8 +3,32 @@ const books = {
   book1: 'The Fellowship',
   book2: 'Two Towers',
   book3: 'Return of the King',
+  publisher: {
+    name: 'Martins Fontes',
+    address: 'Rua XYZ, 123',
+  }
 };
 
 const bookAuthor = {
   authorName: 'J. R. R. Tolkien',
 };
+
+// console.log(books);
+
+Object.assign(books, bookAuthor);
+
+// console.log(books);
+
+// Armazenar as informações de books em um novo objeto
+const newBook = Object.assign({}, books);
+
+newBook.publisher.address = 'Rua ABC';
+
+// console.log(newBook);
+// newBook.book3 = 'Harry Potter';
+
+console.log('New Book');
+console.log(newBook);
+
+console.log('Book');
+console.log(books);
