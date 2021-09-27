b955cb83eda8283e34b6f2bf3854f451eeee589e
Bernardo Salgueiro <bernardo.roll@gmail.com>
2021-09-27 16:15:55 -0300

    Commit inicial

---
+++ assign.js
@@ -0,0 +1,10 @@
+const books = {
+  collection: 'The Lord of the Rings',
+  book1: 'The Fellowship',
+  book2: 'Two Towers',
+  book3: 'Return of the King',
+};
+
+const bookAuthor = {
+  authorName: 'J. R. R. Tolkien',
+};
