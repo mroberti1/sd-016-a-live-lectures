67f3f86740fa7e20d7b4c4f3578db08b4220c1aa
Bernardo Salgueiro <bernardo.roll@gmail.com>
2021-10-07 17:34:44 -0300

    Redução de um array em objeto

--- 2-reduce-object.js
+++ 2-reduce-object.js
@@ -12,3 +12,63 @@ const players = [
 //    'Ronaldo Fenômeno': 'cortedocascao@futebol.br',
 //    'Túlio Maravilha': 'deputado@futebol.br'
 // }
+
+// const obj = {
+//   first: 'value',
+//   'second property': true,
+//   third: 10,
+// }
+// obj['cris'] = 'muito linda';
+// console.log(obj);
+
+const objectPlayers = players.reduce((accumulator, currentValue) => {
+  accumulator[currentValue.fullName] = currentValue.email;
+  return accumulator;
+}, {});
+
+console.log(objectPlayers);
+
+// 1a iteração
+// acc = {}
+// currentValue = { fullName: 'Adriano Imperador', email: 'didico@futebol.br' }
+// retorno -> { 'Adriano Imperador': 'didico@futebol.br' }
+
+// 2a iteração
+// acc = { 'Adriano Imperador': 'didico@futebol.br' }
+// currentValue = { fullName: 'Ronaldinho Gaúcho', email: 'bruxo@futebol.br' }
+// retorno -> 
+// {
+//     'Adriano Imperador': 'didico@futebol.br',
+//     'Ronaldinho Gaúcho', 'bruxo@futebol.br'
+// }
+
+// 3a iteração
+// acc = 
+// {
+//     'Adriano Imperador': 'didico@futebol.br',
+//     'Ronaldinho Gaúcho': 'bruxo@futebol.br'
+// }
+// currentValue = { fullName: 'Ronaldo Fenômeno', email: 'cortedocascao@futebol.br' }
+// retorno -> 
+// {
+//     'Adriano Imperador': 'didico@futebol.br',
+//     'Ronaldinho Gaúcho', 'bruxo@futebol.br',
+//     'Ronaldo Fenômeno': 'cortedocascao@futebol.br',
+// }
+
+// 4a iteração
+// acc = 
+// {
+//     'Adriano Imperador': 'didico@futebol.br',
+//     'Ronaldinho Gaúcho', 'bruxo@futebol.br',
+//     'Ronaldo Fenômeno': 'cortedocascao@futebol.br',
+// }
+// currentValue = { fullName: 'Túlio Maravilha', email: 'deputado@futebol.br' }
+// retorno -> 
+// {
+//     'Adriano Imperador': 'didico@futebol.br',
+//     'Ronaldinho Gaúcho', 'bruxo@futebol.br',
+//     'Ronaldo Fenômeno': 'cortedocascao@futebol.br',
+//     'Túlio Maravilha': 'deputado@futebol.br',
+// }
+
