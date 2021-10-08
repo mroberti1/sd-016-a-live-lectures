ec3e612cd9147e076ef782e3ca9ea690626e6e0f
Bernardo Salgueiro <bernardo.roll@gmail.com>
2021-10-08 17:54:50 -0300

    Destructuring

--- destructuring-1.js
+++ destructuring-1.js
@@ -6,8 +6,15 @@ const singer = {
   albums: ['Minas', 'Geraes', 'Clube da Esquina', 'Canção da America', 'Três Pontas', 'Nascimento', 'Caçador de Mim']
 };
 
-// const phrase = ``;
+// const fullName = singer.fullName;
+// const place = singer.birthplace;
+// const city = singer.city;
+// const albums = singer.albums;
+
+const { fullName, birthplace: place, city, albums, obj } = singer;
+
+const phrase = `O cantor ${fullName} nasceu no ${place} e mora em ${city}. Ele possui ${albums.length} álbuns.`;
 
 // Resultado esperado ao imprimir phrase:
 // 'O cantor Milton Nascimento nasceu no Rio de Janeiro e mora em Belo Horizonte. Ele possui 7 álbuns.
-// console.log(phrase);
+console.log(phrase);
--- destructuring-2.js
+++ destructuring-2.js
@@ -24,20 +24,23 @@ const polarBear = {
   commonName: 'Polar Bear',
   weightRange: {
     min: 350,
-    max: 700
+    max: 700,
   }
 };
 
-const animalDescription = () => {};
+const animalDescription = ({ name, commonName, weightRange }) => {
+  const { min, max, measurementUnit = '' } = weightRange;
+  return `${commonName} (${name}) weighs around ${min}${measurementUnit} - ${max}${measurementUnit}`;
+};
 
 // Resultado esperado ao chamar animalDescription passando o objeto lion:
-// 'Lion (Panghera leo) weighs around 186.55kg - 225kg'
-// console.log(animalDescription(lion));
+// 'Lion (Panthera leo) weighs around 186.55kg - 225kg'
+console.log(animalDescription(lion));
 
 // Resultado esperado ao chamar animalDescription passando o objeto blueWhale:
 // 'Blue whale (Balaenoptera musculus) weighs around 45t - 173t'
-// console.log(animalDescription(blueWhale));
+console.log(animalDescription(blueWhale));
 
 // Resultado esperado ao chamar animalDescription passando o objeto polarBear:
 // 'Polar Bear (Ursus maritimus) weighs around 350 - 700'
-// console.log(animalDescription(polarBear));
+console.log(animalDescription(polarBear));
