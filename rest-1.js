30808b34a83479445872e28496aefb100be6ba0e
Bernardo Salgueiro <bernardo.roll@gmail.com>
2021-10-08 17:17:32 -0300

    Rest operator

--- rest-1.js
+++ rest-1.js
@@ -1,14 +1,16 @@
 // Crie uma função que some todos os parâmetros passados a ela.
-const sumAll = () => {};
+const sumAll = (...values) => {
+  return values.reduce((accumulator, currentValue) => accumulator + currentValue);
+};
 
 // Resultado esperado ao passar 1 e 2 como parâmetro para a função: 3
 // ex: sumAll(1, 2) -> 3
-// console.log(sumAll(1, 2));
+console.log(sumAll(1, 2));
 
 // Resultado esperado ao passar 1, 2 e 3 como parâmetro para a função: 6
 // ex: sumAll(1, 2, 3) -> 6
-// console.log(sumAll(1, 2, 3));
+console.log(sumAll(1, 2, 3));
 
 // Resultado esperado ao passar 1, 2, 3, 4 e 5 como parâmetro para a função: 15
 // ex: sumAll(1, 2, 3, 4, 5) -> 15
-// console.log(sumAll(1, 2, 3, 4, 5));
+console.log(sumAll(1, 2, 3, 4, 5));
--- rest-2.js
+++ rest-2.js
@@ -1,10 +1,12 @@
 // Faça uma função que obtenha seis dados de data e hora 
 // (ano, mês, dia, hora, minuto, segundo) e retorne os dados de ano, mês e dia formatados (dd/MM/AAAA).
 
-const formatDate = () => {};
+const formatDate = (year, month, day, ...otherParameters) => {
+  console.log(otherParameters);
+  return `${day}/${month}/${year}`;
+};
 
 // Ao chamar a função formatDate com os seguintes parâmetros:
 // '1981', '7', '31', '22', '47', '12'
 // O resultado esperado é: 31/7/1981
-// console.log(formatDate('1981', '7', '31', '22', '47', '12'));
-
+console.log(formatDate('1981', '7', '31', '22', '47', '12'));
